c3898009d2aabf6edfc5461efcce6f74e94a074e
Sara Rossow <rossowka@icloud.com>
2021-07-08 13:42:02 +0200

    linked DOM elements with variables

---
+++ index.js
@@ -0,0 +1,4 @@
+const convertBtn = document.getElementById('covert-btn');
+const inputAmount = document.getElementById('input-amount');
+const currencySelector = document.getElementById('select-currency');
+const inputConverted = document.getElementById('input-converted');
