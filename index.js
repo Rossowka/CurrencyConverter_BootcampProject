b3bc4fbfc789167c8429bb16b45c2bdb39bdd326
Sara Rossow <rossowka@icloud.com>
2021-07-08 13:44:06 +0200

    added global variable

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const convertBtn = document.getElementById('covert-btn');
 const inputAmount = document.getElementById('input-amount');
 const currencySelector = document.getElementById('select-currency');
 const inputConverted = document.getElementById('input-converted');
+
+let currency = currencySelector.value
+
